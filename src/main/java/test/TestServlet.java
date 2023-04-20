7e0b6805fd0755ba78e759b2be3740a802cbaab5
Tiv <rabbit881030@gmail.com>
2023-04-20 11:52:39 +0800

    add !

--- src/main/java/test/TestServlet.java
+++ src/main/java/test/TestServlet.java
@@ -13,7 +13,7 @@ public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
    	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("testtest");
+		response.getWriter().append("testtest!");
 	}
 
 }
